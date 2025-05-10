e2705c6e51a2768895f3577a45c0ebe59283ee3b
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-05-10 10:12:03 +0700

    Menyelesaikan fitur login dan register

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:chat_app/components/rounded_button.dart';
 import 'chat_screen.dart';
 import 'package:chat_app/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -10,6 +11,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _auth = FirebaseAuth.instance;
+  String? email;
+  String? password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,8 +36,9 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,
               onChanged: (value) {
-                //Do something with the user input.
+                email = value;
               },
               decoration: kInputDecoration.copyWith(hintText: 'Enter your email'),
             ),
@@ -40,8 +46,9 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 8.0,
             ),
             TextField(
+              obscureText: true,
               onChanged: (value) {
-                //Do something with the user input.
+                password = value;
               },
               decoration: kInputDecoration.copyWith(hintText: 'Enter your password'),
             ),
@@ -51,8 +58,16 @@ class _LoginScreenState extends State<LoginScreen> {
             RoundedButton(
               warna: Colors.lightBlueAccent,
               judul: 'Log In',
-              onPressed: (){
-                Navigator.pushNamed(context, ChatScreen.id);
+              onPressed: () async{
+                try{
+                  final newUser = await _auth.signInWithEmailAndPassword(email: email!, password: password!);
+                  if(newUser != null){
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                } catch(e) {
+
+                }
+
               },
             ),
           ],
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:chat_app/components/rounded_button.dart';
 import 'chat_screen.dart';
 import 'package:chat_app/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -10,6 +11,10 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  String? email;
+  String? password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,8 +36,9 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,
               onChanged: (value) {
-                //Do something with the user input.
+                email = value;
               },
               decoration: kInputDecoration.copyWith(hintText: 'Enter your email'),
             ),
@@ -40,8 +46,9 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 8.0,
             ),
             TextField(
+              obscureText: true,
               onChanged: (value) {
-                //Do something with the user input.
+                password = value;
               },
               decoration: kInputDecoration.copyWith(hintText: 'Enter your password'),
             ),
@@ -51,9 +58,17 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             RoundedButton(
               warna: Colors.blueAccent,
               judul: 'Register',
-              onPressed: (){
-                Navigator.pushNamed(context, ChatScreen.id);
-              },
+                onPressed: () async {
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email!, password: password!);
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
+                },
             ),
           ],
         ),
